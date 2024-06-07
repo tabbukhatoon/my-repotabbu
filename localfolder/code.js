d94a147e01724687b839414c207eb3a8277fc471
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-07 20:48:26 +0530

    add some feature

--- localfolder/code.js
+++ localfolder/code.js
@@ -1,5 +1,16 @@
 let button=document.querySelector(".button");
+let body=document.querySelector("body");
+let mode='dark';
 console.log(button);
 button.addEventListener("click",()=>{
     console.log('button was clicked');
+if(mode==='dark'){
+    mode='light';
+    body.classList.add('light');
+    body.classList.remove('dark');
+}
+else{
+    body.classList.add("dark");
+    body.classList.remove('light');
+}
 });
