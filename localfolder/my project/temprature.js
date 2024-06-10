f11bb54251905fb66326642a12117ed479dde7c6
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-10 18:04:06 +0530

    javascript structure

---
+++ localfolder/my project/temprature.js
@@ -0,0 +1,4 @@
+let celsius=document.querySelector(".celsius");
+let kelvin=document.querySelector(".kelvin");
+let farenheit=document.querySelector(".farenheit");
+console.log(celsius,kelvin,farenheit);
