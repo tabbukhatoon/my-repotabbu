927d93aa35fd1c572590829b2e896a216f246e66
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-10 22:19:13 +0530

    add javascript structure

---
+++ localfolder/my project/vowel-counter.js
@@ -0,0 +1,10 @@
+let input=document.querySelector("#input");
+let button=document.querySelector(".button");
+let msg=document.querySelector(".msg");
+console.log(input,button,msg);
+button.addEventListener("click",()=>{
+    console.log('button was click');
+
+
+    
+})
