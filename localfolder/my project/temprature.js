3e6da5176ec8ed68bea219cb577821b00b48055b
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-10 18:19:57 +0530

    add celsius structure

--- localfolder/my project/temprature.js
+++ localfolder/my project/temprature.js
@@ -2,3 +2,13 @@ let celsius=document.querySelector(".celsius");
 let kelvin=document.querySelector(".kelvin");
 let farenheit=document.querySelector(".farenheit");
 console.log(celsius,kelvin,farenheit);
+//celsius to farenheit=(0c * 9/5)+32=32f;
+//clesius to kelvin =0c+273.15k;
+celsius.addEventListener("input",()=>{
+let c=parseFloat(celsius.value);
+let f= (c*9/5)+32;
+let k=c+273.15;
+farenheit.value=f;
+kelvin.value=k;
+});
+//
