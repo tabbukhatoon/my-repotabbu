5844845b595d9f1e30e386c8c901ff8483873b85
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-13 13:15:00 +0530

    add code

--- localfolder/my project/mode.js
+++ localfolder/my project/mode.js
@@ -1,3 +1,17 @@
 let button=document.querySelector(".mode");
 let body=document.querySelector("body");
 console.log(button,body);
+let  mode='dark';
+button.addEventListener("click",()=>{
+    console.log("button was clicked");
+if(mode==='dark'){
+    mode='light'
+    body.classList.add("light");
+    body.classList.remove("dark");
+}
+else{
+    mode='dark';
+    body.classList.add("dark");
+    body.classList.remove("light");
+}
+})
