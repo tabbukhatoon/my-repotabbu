e946f067205521ab18c911b52d56e0228c819eea
tabbukhatoon <yunushhasmi6@gmail.com>
2024-06-13 08:55:59 +0530

    add javascript code

--- localfolder/my project/vowel-counter.js
+++ localfolder/my project/vowel-counter.js
@@ -4,7 +4,20 @@ let msg=document.querySelector(".msg");
 console.log(input,button,msg);
 button.addEventListener("click",()=>{
     console.log('button was click');
-
+let val=input.value;
+console.log(val);
+if(val=== ''){
+    alert("please enter a text");
+}
+else{
+    let score=0;
+    for(i=0, i<val.length; i++;) {
+        if(val[i]==="a" && val[i]==="u" && val[i]==="o" && val[i]==="i" && val[i]==="e" && )
+        {
+            score++;
+        }
+    }
+}
 
     
 })
